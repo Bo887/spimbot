d29414ba048ed7a9ed73586df120611433032206
Eric Zhang <ezhang887@gmail.com>
2019-04-16 15:04:51 -0500

    cleanup start code + add run script

---
+++ Makefile
@@ -0,0 +1,14 @@
+default:
+	QtSpimbot -tournament -file spimbot.s -file2 spimbot.s
+
+seed-233:
+	QtSpimbot -tournament -file spimbot.s -file2 spimbot.s -mapseed 233
+
+seed-1:
+	QtSpimbot -tournament -file spimbot.s -file2 spimbot.s -mapseed 1
+
+seed-8:
+	QtSpimbot -tournament -file spimbot.s -file2 spimbot.s -mapseed 8
+
+seed-16:
+	QtSpimbot -tournament -file spimbot.s -file2 spimbot.s -mapseed 16
--- spimbot.s
+++ spimbot.s
@@ -14,11 +14,11 @@ BOT_Y                   = 0xffff0024
 
 TIMER                   = 0xffff001c
 
-SUBMIT_ORDER 			= 0xffff00b0
-DROPOFF 				= 0xffff00c0
-PICKUP 					= 0xffff00e0
-GET_TILE_INFO			= 0xffff0050
-SET_TILE				= 0xffff0058
+SUBMIT_ORDER 		= 0xffff00b0
+DROPOFF 		= 0xffff00c0
+PICKUP 			= 0xffff00e0
+GET_TILE_INFO		= 0xffff0050
+SET_TILE		= 0xffff0058
 
 REQUEST_PUZZLE          = 0xffff00d0
 SUBMIT_SOLUTION         = 0xffff00d4
@@ -33,110 +33,110 @@ REQUEST_PUZZLE_INT_MASK = 0x800
 REQUEST_PUZZLE_ACK      = 0xffff00d8
 
 GET_MONEY               = 0xffff00e4
-GET_LAYOUT 				= 0xffff00ec
-SET_REQUEST 			= 0xffff00f0
-GET_REQUEST 			= 0xffff00f4
+GET_LAYOUT 		= 0xffff00ec
+SET_REQUEST 		= 0xffff00f0
+GET_REQUEST 	        = 0xffff00f4
 
-GET_INVENTORY 			= 0xffff0040
-GET_TURNIN_ORDER 		= 0xffff0044
-GET_TURNIN_USERS		= 0xffff0048
-GET_SHARED 				= 0xffff004c
+GET_INVENTORY 		= 0xffff0040
+GET_TURNIN_ORDER 	= 0xffff0044
+GET_TURNIN_USERS	= 0xffff0048
+GET_SHARED 		= 0xffff004c
 
-GET_BOOST 				= 0xffff0070
+GET_BOOST 		= 0xffff0070
 GET_INGREDIENT_INSTANT 	= 0xffff0074
-FINISH_APPLIANCE_INSTANT = 0xffff0078
+FINISH_APPLIANCE_INSTANT= 0xffff0078
 
-puzzle:      .word 0:452
+puzzle:             .word 0:452
 
 .text
 main:
 	# Construct interrupt mask
-	li      $t4, 0
-	or      $t4, $t4, BONK_INT_MASK # request bonk
-	or      $t4, $t4, REQUEST_PUZZLE_INT_MASK	        # puzzle interrupt bit
-	or      $t4, $t4, 1 # global enable
-	mtc0    $t4, $12
+	li          $t4, 0
+	or          $t4, $t4, BONK_INT_MASK # request bonk
+	or          $t4, $t4, REQUEST_PUZZLE_INT_MASK	        # puzzle interrupt bit
+	or          $t4, $t4, 1 # global enable
+	mtc0        $t4, $12
 	
 	#Fill in your code here
 infinite:
-	j infinite
+	j           infinite
 
 .kdata
-chunkIH:    .space 32
-non_intrpt_str:    .asciiz "Non-interrupt exception\n"
-unhandled_str:    .asciiz "Unhandled interrupt type\n"
+chunkIH:            .space 32
+non_intrpt_str:     .asciiz "Non-interrupt exception\n"
+unhandled_str:      .asciiz "Unhandled interrupt type\n"
 .ktext 0x80000180
 interrupt_handler:
 .set noat
-        move      $k1, $at        # Save $at
+        move        $k1, $at        # Save $at
 .set at
-        la        $k0, chunkIH
-        sw        $a0, 0($k0)        # Get some free registers
-        sw        $v0, 4($k0)        # by storing them to a global variable
-        sw        $t0, 8($k0)
-        sw        $t1, 12($k0)
-        sw        $t2, 16($k0)
-        sw        $t3, 20($k0)
-		sw $t4, 24($k0)
-		sw $t5, 28($k0)
+        la          $k0, chunkIH
+        sw          $a0, 0($k0)        # Get some free registers
+        sw          $v0, 4($k0)        # by storing them to a global variable
+        sw          $t0, 8($k0)
+        sw          $t1, 12($k0)
+        sw          $t2, 16($k0)
+        sw          $t3, 20($k0)
+	sw          $t4, 24($k0)
+	sw          $t5, 28($k0)
 
-        mfc0      $k0, $13             # Get Cause register
-        srl       $a0, $k0, 2
-        and       $a0, $a0, 0xf        # ExcCode field
-        bne       $a0, 0, non_intrpt
+        mfc0        $k0, $13             # Get Cause register
+        srl         $a0, $k0, 2
+        and         $a0, $a0, 0xf        # ExcCode field
+        bne         $a0, 0, non_intrpt
 
 
 
 interrupt_dispatch:            # Interrupt:
-    mfc0       $k0, $13        # Get Cause register, again
-    beq        $k0, 0, done        # handled all outstanding interrupts
+        mfc0        $k0, $13        # Get Cause register, again
+        beq         $k0, 0, done        # handled all outstanding interrupts
 
-    and        $a0, $k0, BONK_INT_MASK    # is there a bonk interrupt?
-    bne        $a0, 0, bonk_interrupt
+        and         $a0, $k0, BONK_INT_MASK    # is there a bonk interrupt?
+        bne         $a0, 0, bonk_interrupt
 
-    and        $a0, $k0, TIMER_INT_MASK    # is there a timer interrupt?
-    bne        $a0, 0, timer_interrupt
+        and         $a0, $k0, TIMER_INT_MASK    # is there a timer interrupt?
+        bne         $a0, 0, timer_interrupt
 
-	and 	$a0, $k0, REQUEST_PUZZLE_INT_MASK
-	bne 	$a0, 0, request_puzzle_interrupt
+        and 	    $a0, $k0, REQUEST_PUZZLE_INT_MASK
+        bne 	    $a0, 0, request_puzzle_interrupt
 
-    li        $v0, PRINT_STRING    # Unhandled interrupt types
-    la        $a0, unhandled_str
-    syscall
-    j    done
+        li          $v0, PRINT_STRING    # Unhandled interrupt types
+        la          $a0, unhandled_str
+        syscall
+        j           done
 
 bonk_interrupt:
-	sw 		$0, BONK_ACK
-    #Fill in your code here
-    j       interrupt_dispatch    # see if other interrupts are waiting
+	sw 	    $0, BONK_ACK
+        #Fill in your code here
+        j           interrupt_dispatch    # see if other interrupts are waiting
 
 request_puzzle_interrupt:
-	sw 		$0, REQUEST_PUZZLE_ACK
+	sw 	    $0, REQUEST_PUZZLE_ACK
 	#Fill in your code here
-	j	interrupt_dispatch
+	j	    interrupt_dispatch
 
 timer_interrupt:
-	sw 		$0, TIMER_ACK
+	sw 	    $0, TIMER_ACK
 	#Fill in your code here
-    j        interrupt_dispatch    # see if other interrupts are waiting
+        j           interrupt_dispatch    # see if other interrupts are waiting
 
 non_intrpt:                # was some non-interrupt
-    li        $v0, PRINT_STRING
-    la        $a0, non_intrpt_str
-    syscall                # print out an error message
-    # fall through to done
+        li          $v0, PRINT_STRING
+        la          $a0, non_intrpt_str
+        syscall                # print out an error message
+        # fall through to done
 
 done:
-    la      $k0, chunkIH
-    lw      $a0, 0($k0)        # Restore saved registers
-    lw      $v0, 4($k0)
-	lw      $t0, 8($k0)
-    lw      $t1, 12($k0)
-    lw      $t2, 16($k0)
-    lw      $t3, 20($k0)
-	lw $t4, 24($k0)
-	lw $t5, 28($k0)
+        la          $k0, chunkIH
+        lw          $a0, 0($k0)        # Restore saved registers
+        lw          $v0, 4($k0)
+        lw          $t0, 8($k0)
+        lw          $t1, 12($k0)
+        lw          $t2, 16($k0)
+        lw          $t3, 20($k0)
+        lw          $t4, 24($k0)
+        lw          $t5, 28($k0)
 .set noat
-    move    $at, $k1        # Restore $at
+        move        $at, $k1        # Restore $at
 .set at
-    eret
+        eret
