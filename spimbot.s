37d349a189a6b43a466046b8427d17a073838265
Ben Nordick <nordick2@illinois.edu>
2019-04-17 21:20:04 -0500

    Test puzzle generation and solving time

--- spimbot.s
+++ spimbot.s
@@ -46,31 +46,364 @@ GET_BOOST 		= 0xffff0070
 GET_INGREDIENT_INSTANT 	= 0xffff0074
 FINISH_APPLIANCE_INSTANT= 0xffff0078
 
-puzzle:             .word 0:452
+puzzle:			.space 1808
+d_puzzle_pending:	.word 0
 
-PI:                 .float 3.14
-three:              .float 3.0
-five:               .float 5.0
-F180:               .float 180.0
+PI:			.float 3.14
+three:			.float 3.0
+five:			.float 5.0
+F180:			.float 180.0
 
 .text
+
+# -----------------------------------------------------------------------
+# main function - entry point
+# -----------------------------------------------------------------------
+
 main:
 	# Construct interrupt mask
-	li          $t4, 0
-	or          $t4, $t4, BONK_INT_MASK # request bonk
-	or          $t4, $t4, REQUEST_PUZZLE_INT_MASK	        # puzzle interrupt bit
-	or          $t4, $t4, 1 # global enable
-	mtc0        $t4, $12
+	li	$t4, 0
+	or	$t4, $t4, BONK_INT_MASK			# request bonk
+	or	$t4, $t4, REQUEST_PUZZLE_INT_MASK	# puzzle interrupt bit
+	or	$t4, $t4, 1				# global enable
+	mtc0	$t4, $12
 	
-	#Fill in your code here
-        #testing the given atan2 function right now.....
-        li          $a0, 1
-        li          $a1, 2
-        jal         sb_arctan
-        move        $t9, $v0        #t9 should show around 63 (0x3F) degrees
-        
+	# Fill in your code here
+	
+	# figure out which side we're on
+	lw	$t0, BOT_X
+	bge	$t0, 100, infinite	# only test on one bot
+	
+	# testing puzzle generation and solving time
+	lw	$s0, TIMER
+	la	$t0, puzzle
+        sw	$t0, REQUEST_PUZZLE
+	
 infinite:
-	j           infinite
+	lbu	$t0, d_puzzle_pending	# will be set in kernel mode when puzzle interrupt occurs
+	beq	$t0, $zero, m_no_puzzle
+	
+	lw	$t0, TIMER
+	sub	$a0, $t0, $s0		# find how long it took to get the puzzle
+	li	$v0, PRINT_INT
+	syscall
+	li	$a0, '\n'
+	li	$v0, PRINT_CHAR
+	syscall
+	
+	sb	$zero, d_puzzle_pending
+	la	$a0, puzzle
+	jal	islandfill
+	la	$t0, puzzle
+	sw	$t0, SUBMIT_SOLUTION
+	
+	lw	$s0, TIMER
+	sw	$t0, REQUEST_PUZZLE	# get another puzzle
+	
+m_no_puzzle:
+	j	infinite
+	
+# -----------------------------------------------------------------------
+# puzzle solving functions
+# -----------------------------------------------------------------------
+
+#struct puzzle {
+#	unsigned num_rows;
+#	unsigned num_cols;
+#   unsigned char unsolved_puzzle[num_rows][num_cols];
+#   unsigned char puzzle_bitmap[num_rows][ceil(num_cols/8)];
+#} puzzle;
+
+# char floodfill (Puzzle* puzzle, char marker, int row, int col) {
+#       if (row < 0 || col < 0) {
+#             return marker;
+#       }
+#       if (row >= puzzle->NUM_ROWS || col >= puzzle->NUM_COLS) {
+#               return marker;
+#       }
+#       char ** board = puzzle->board;
+#       if (board[row][col] != '#') {
+#               return marker;
+#       }
+#       board[row][col] = marker;
+#       floodfill(puzzle, marker, row + 1, col + 1);
+#       floodfill(puzzle, marker, row + 1, col + 0);
+#       floodfill(puzzle, marker, row + 1, col - 1);
+#       floodfill(puzzle, marker, row, col + 1);
+#       floodfill(puzzle, marker, row, col - 1);
+#       floodfill(puzzle, marker, row - 1, col + 1);
+#       floodfill(puzzle, marker, row - 1, col + 0);
+#       floodfill(puzzle, marker, row - 1, col - 1);
+#       return marker + 1;
+# }
+# void islandfill(Puzzle* puzzle) {
+#       char marker = '#';
+#       for (int i = 0; i < puzzle->NUM_ROWS; i++) {
+#             for (int j = 0; j < puzzle->NUM_COLS; j++) {
+#                     marker = floodfill(puzzle,marker,i,j);
+#             }
+#       }
+# }
+
+floodfill:
+
+#       if (row < 0 || col < 0) {
+#             return marker;
+#       }
+#       if (row >= puzzle->NUM_ROWS || col >= puzzle->NUM_COLS) {
+#               return marker;
+#       }
+        slt     $t0, $a2, 0
+        slt     $t1, $a3, 0
+        or      $t0, $t1, $t0
+        beq     $t0, 0, f_end_if1
+        move    $v0, $a1
+        jr      $ra
+f_end_if1:
+
+        lw      $t0, 0($a0)
+        lw      $t1, 4($a0)
+        sge     $t0, $a2, $t0
+        sge     $t1, $a3, $t1
+        or      $t0, $t1, $t0
+
+        beq     $t0, 0, f_end_if2
+        move    $v0, $a1
+        jr      $ra
+f_end_if2:
+
+
+#       char board[][] = puzzle->board;
+#       if (board[row][col] != '#') {
+#               return marker;
+#       }
+        lw      $t0, 0($a0)
+        lw      $t1, 4($a0)
+        mul     $t2, $a2, $t1
+        add     $t2, $t2, $a3
+        add     $t2, $t2, $a0
+        add     $t2, $t2, 8
+        lb      $t3, 0($t2)
+
+        beq     $t3, '#', f_endif_3
+        move    $v0, $a1
+        jr      $ra
+f_endif_3:
+
+f_recur:
+        sub     $sp, $sp, 88
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+        sw      $s3, 16($sp)
+        sw      $a0, 76($sp)
+        sw      $a1, 80($sp)
+        sw      $a2, 84($sp)
+
+#       board[row][col] = marker;
+        sb      $a1, 0($t2)
+
+        move    $s0, $a0
+        move    $s1, $a1
+        move    $s2, $a2
+        move    $s3, $a3
+
+#       floodfill(puzzle, marker, row + 1, col + 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, 1
+        add     $a3, $s3, 1
+        jal     floodfill
+
+#       floodfill(puzzle, marker, row + 1, col + 0);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, 1
+        add     $a3, $s3, 0
+        jal     floodfill
+#       floodfill(puzzle, marker, row + 1, col - 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, 1
+        add     $a3, $s3, -1
+        jal     floodfill
+#       floodfill(puzzle, marker, row, col + 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, 0
+        add     $a3, $s3, 1
+        jal     floodfill
+#       floodfill(puzzle, marker, row, col - 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, 0
+        add     $a3, $s3, -1
+        jal     floodfill
+#       floodfill(puzzle, marker, row - 1, col + 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, -1
+        add     $a3, $s3, 1
+        jal     floodfill
+#       floodfill(puzzle, marker, row - 1, col + 0);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, -1
+        add     $a3, $s3, 0
+        jal     floodfill
+#       floodfill(puzzle, marker, row - 1, col - 1);
+        move    $a0, $s0
+        move    $a1, $s1
+        add     $a2, $s2, -1
+        add     $a3, $s3, -1
+        jal     floodfill
+#       return marker + 1;
+        add     $v0, $a1, 1
+f_done:
+	lw      $ra, 0($sp)
+	lw      $s0, 4($sp)
+	lw      $s1, 8($sp)
+	lw      $s2, 12($sp)
+	lw      $s3, 16($sp)
+
+	lw      $a0, 76($sp)
+	lw      $a1, 80($sp)
+	lw      $a2, 84($sp)
+	add     $sp, $sp, 88
+
+	jr      $ra
+
+# void islandfill(Puzzle* puzzle) {
+#       char marker = '#';
+#       for (int i = 0; i < puzzle->NUM_ROWS; i++) {
+#             for (int j = 0; j < puzzle->NUM_COLS; j++) {
+#                     marker = floodfill(puzzle,marker,i,j);
+#             }
+#       }
+# }
+
+islandfill:
+        sub     $sp, $sp, 88
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+        sw      $s3, 16($sp)
+        sw      $s4, 20($sp)
+        sw      $s5, 24($sp)
+        sw      $s6, 28($sp)
+        sw      $s7, 32($sp)
+        sw      $a0, 76($sp)
+        sw      $a1, 80($sp)
+        sw      $a2, 84($sp)
+	
+	lw	$s6, TIMER	# measure how long it takes to solve
+
+        move    $s0, $a0
+        li      $s1, 'A'
+        li      $s2, 0
+
+        lw      $s4, 0($a0)
+        lw      $s5, 4($a0)
+
+i_outer_loop:
+        bge     $s2, $s4, i_outer_end
+
+        li      $s3, 0
+i_inner_loop:
+        bge     $s3, $s5, i_inner_end
+
+        # marker = floodfill(puzzle,marker,i,j);
+        move    $a0, $s0
+        move    $a1, $s1
+        move    $a2, $s2
+        move    $a3, $s3
+        jal     floodfill
+        move    $s1, $v0
+
+        add     $s3, $s3, 1
+        j       i_inner_loop
+i_inner_end:
+
+        add     $s2, $s2, 1
+        j       i_outer_loop
+i_outer_end:
+	lw	$s7, TIMER	# find how much time passed
+	sub	$a0, $s7, $s6
+	li	$v0, PRINT_INT
+	syscall
+	li	$a0, '\n'
+	li	$v0, PRINT_CHAR
+	syscall
+	syscall
+
+	#move    $a0, $s0
+	#jal     print_board
+
+        lw      $ra, 0($sp)
+        lw      $s0, 4($sp)
+        lw      $s1, 8($sp)
+        lw      $s2, 12($sp)
+        lw      $s3, 16($sp)
+        lw      $s4, 20($sp)
+        lw      $s5, 24($sp)
+        lw      $s6, 28($sp)
+        lw      $s7, 32($sp)
+
+        lw      $a0, 76($sp)
+        lw      $a1, 80($sp)
+        lw      $a2, 84($sp)
+        add     $sp, $sp, 88
+        jr      $ra
+
+
+# print board ##################################################
+#
+# argument $a0: board to print
+.globl print_board
+print_board:
+    sub         $sp, $sp, 20
+    sw          $ra, 0($sp)     # save $ra and free up 4 $s registers for
+    sw          $s0, 4($sp)     # i
+    sw          $s1, 8($sp)     # j
+    sw          $s2, 12($sp)    # the address
+    sw          $s3, 16($sp)    # the line number
+    move        $s2, $a0
+    li          $s0, 0          # i
+pb_loop1:
+    li          $s1, 0          # j
+pb_loop2:
+
+    lw          $t0, 0($s2)     # NUM_ROWS
+    lw          $t1, 4($s2)     # NUM_COLS
+
+    mul         $t2, $s0, $t1   # i * NUM_COLS
+    add         $t2, $t2, $s1   # i * NUM_COLS + j
+    add         $t2, $t2, 8
+    add         $t2, $t2, $s2
+
+
+    lb          $a0, 0($t2)     # num = &board[i][j]
+    li          $v0, 11
+    syscall
+    j           pb_cont
+pb_cont:
+    add         $s1, $s1, 1     # j++
+    blt         $s1, 8, pb_loop2
+    li          $v0, 11         # at the end of a line, print a newline char.
+    li          $a0, '\n'
+    syscall
+
+    add         $s0, $s0, 1     # i++
+    blt         $s0, 8, pb_loop1
+    lw          $ra, 0($sp)     # restore registers and return
+    lw          $s0, 4($sp)
+    lw          $s1, 8($sp)
+    lw          $s2, 12($sp)
+    lw          $s3, 16($sp)
+    add         $sp, $sp, 20
+    jr          $ra
 
 # -----------------------------------------------------------------------
 # sb_arctan - computes the arctangent of y / x
@@ -183,9 +516,10 @@ bonk_interrupt:
         j           interrupt_dispatch    # see if other interrupts are waiting
 
 request_puzzle_interrupt:
-	sw 	    $0, REQUEST_PUZZLE_ACK
-	#Fill in your code here
-	j	    interrupt_dispatch
+	sw	$0, REQUEST_PUZZLE_ACK
+	li	$t0, 1
+	sb	$t0, d_puzzle_pending
+	j	interrupt_dispatch
 
 timer_interrupt:
 	sw 	    $0, TIMER_ACK
